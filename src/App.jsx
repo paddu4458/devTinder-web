63af81b98b25184b0d6cbc6ad2c658161432d0b4
Pradeep Kumar <pradeep4458@gmail.com>
2025-05-16 21:57:43 +0530

    changes related to new code

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+function App() {
+
+  return (
+    <>
+      <div>
+      <h1 className="underline font-bold">Vite + React</h1>
+       
+      </div>
+     
+    </>
+  )
+}
+
+export default App
