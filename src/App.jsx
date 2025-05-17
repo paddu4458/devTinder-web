d9206bd94dd7e07f358d509f58dba338a8a0cace
Pradeep Kumar <pradeep4458@gmail.com>
2025-05-17 17:36:15 +0530

    set my redux store and login page for the dev tinder application changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,27 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import Body from "./components/Body"
+import Login from "./components/Login"
+import Feed from "./components/Feed"
+import Profile from "./components/Profile";
+import { Provider } from "react-redux";
+import appStore from "./utils/appStore";
 
 function App() {
-
-  return (
-    <>
-      <div>
-      <h1 className="underline font-bold">Vite + React</h1>
-       
-      </div>
-     
-    </>
-  )
+return (
+<>
+  <Provider store={appStore}>
+      <BrowserRouter basename="/">  
+        <Routes>
+          <Route path="/" element={<Body />}> 
+              <Route path="/" element={<Feed />} /> 
+              <Route path="/login" element={<Login />} /> 
+              <Route path="/profile" element={<Profile />} /> 
+          </Route>  
+        </Routes>
+      </BrowserRouter>
+  </Provider>
+</>
+  );
 }
 
 export default App
---
+++ src/components/Login.jsx
@@ -0,0 +1,56 @@
+import { useState } from "react";
+import axios from "axios";
+import { useDispatch } from "react-redux";
+import { addUser } from "../utils/userSlice";
+import { useNavigate } from "react-router-dom";
+import { BASE_URL } from "../utils/constants";
+
+const Login= () =>{
+
+    const [emailId, setEmailId] = useState("deepak123@gmail.com");
+    const [password, setPassword] = useState("Deepak@123");
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+
+    const handleLogin = async () =>{
+      try {
+         const res = await axios.post(
+          BASE_URL + "/login", {
+            emailId,
+            password,
+        }, {withCredentials: true});
+      dispatch(addUser(res.data));
+      return navigate("/");
+      } 
+      catch(err){
+        console.error(err);
+      }
+    };
+
+    return (
+        <>
+        <div className="flex justify-center my-10">
+            <div className="card bg-base-100 w-96 shadow-sm">
+  <div className="card-body">
+    <h2 className="card-title justify-center">Login</h2>
+    <div>
+<fieldset className="fieldset">
+  <legend className="fieldset-legend">Email Id </legend>
+  <input type="text" value={emailId} className="input" onChange={(e) => setEmailId(e.target.value)} placeholder="Email Id" />
+
+  <legend className="fieldset-legend">Password</legend>
+  <input type="password" value={password} className="input" onChange={(e) => setPassword(e.target.value)} placeholder="Password" />
+
+</fieldset>
+    </div>
+    <div className="card-actions justify-center my-4">
+      <button className="btn btn-primary" onClick={handleLogin}>Login</button>
+    </div>
+  </div>
+</div>
+        </div>
+        </>
+    );
+}
+
+export default Login;
